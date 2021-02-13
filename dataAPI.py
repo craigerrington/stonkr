f45a3195d4be61a5ae434e918717afdd98f8fe1c
Craig Errington <craigerrington@gmail.com>
2021-02-13 04:18:36 +0000

    removing old API

--- dataAPI.py
+++ dataAPI.py
@@ -9,12 +9,9 @@ def getTickerData(stonks):
     tickerString = ""
 
     for c in stonks:
-        #url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&apikey=U0CROYL4SY1FJ1VS'.format(c)
         url = 'https://finnhub.io/api/v1/quote?symbol={}&token=***REMOVED***'.format(c)
         data = urllib2.urlopen(url).read()
         data = json.loads(data)
-        #price = data['Global Quote']['05. price']
-        #pct_ch = data['Global Quote']['10. change percent']
         price = data['c']
         prevclose = data['pc']
         pct_ch = "%.2f" % ((price/ prevclose -1)) * 100 + '%'
